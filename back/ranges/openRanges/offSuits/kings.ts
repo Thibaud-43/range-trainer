d1a7415a03c84a60ab464d2e9b14dea4776098b6
Rouchon <rouchon@MacBook-Air-de-Rouchon.local>
2022-11-20 10:33:55 +0100

    add kings offsuit ranges

---
+++ back/ranges/openRanges/offSuits/kings.ts
@@ -0,0 +1,279 @@
+import { Range } from "../types";
+
+const pairs: Range[] = [
+  {
+    hand: { firstCard: "K", secondCard: "Q", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "open",
+      },
+      {
+        position: "cut-off",
+        action: "open",
+      },
+      {
+        position: "hi-jack",
+        action: "open",
+      },
+      {
+        position: "lo-jack",
+        action: "open",
+      },
+      {
+        position: "small-blind",
+        action: "open",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "J", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "open",
+      },
+      {
+        position: "cut-off",
+        action: "open",
+      },
+      {
+        position: "hi-jack",
+        action: "open",
+      },
+      {
+        position: "lo-jack",
+        action: "open",
+      },
+      {
+        position: "small-blind",
+        action: "open",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "10", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "open",
+      },
+      {
+        position: "cut-off",
+        action: "open",
+      },
+      {
+        position: "hi-jack",
+        action: "open",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "open",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "9", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "open",
+      },
+      {
+        position: "cut-off",
+        action: "open",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "open",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "8", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "open",
+      },
+      {
+        position: "cut-off",
+        action: "fold",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "call",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "7", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "fold",
+      },
+      {
+        position: "cut-off",
+        action: "fold",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "call",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "6", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "fold",
+      },
+      {
+        position: "cut-off",
+        action: "fold",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "call",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "5", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "fold",
+      },
+      {
+        position: "cut-off",
+        action: "fold",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "call",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "4", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "fold",
+      },
+      {
+        position: "cut-off",
+        action: "fold",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "call",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "3", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "fold",
+      },
+      {
+        position: "cut-off",
+        action: "fold",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "call",
+      },
+    ],
+  },
+  {
+    hand: { firstCard: "K", secondCard: "2", suited: false },
+    actionsByPositions: [
+      {
+        position: "button",
+        action: "fold",
+      },
+      {
+        position: "cut-off",
+        action: "fold",
+      },
+      {
+        position: "hi-jack",
+        action: "fold",
+      },
+      {
+        position: "lo-jack",
+        action: "fold",
+      },
+      {
+        position: "small-blind",
+        action: "call",
+      },
+    ],
+  },
+];
